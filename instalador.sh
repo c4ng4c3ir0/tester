1ffeff5e6bf74d5fc3165e70dffec44722d46602
c4ng4c3ir0 (João Victor) <54048948+c4ng4c3ir0@users.noreply.github.com>
2021-05-04 15:18:57 -0300

    Create instalador.sh

---
+++ instalador.sh
@@ -0,0 +1,23 @@
+echo "instalando gauplus"
+echo 
+GO111MODULE=on go get -u -v github.com/bp0lr/gauplus;
+echo
+echo "gauplus instalado"
+echo
+echo "Instalando dalfox..."
+echo
+GO111MODULE=on go get -v github.com/hahwul/dalfox/v2;
+echo "Dalfox instalado!"
+echo
+echo "Instalando Anew"
+echo
+go get -u github.com/tomnomnom/anew;
+echo
+echo "Anew instalado";
+echo
+echo
+echo "Finalizando Instalação"
+cd ;
+cd go/bin;
+sudo mv * /usr/bin;
+echo "Instalação Finalizada!!"
